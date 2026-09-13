498827a20434a60df5240a2e83d2e228e4a4ad5b
Mohit <mohit1357913579@gmail.com>
2026-09-13 19:27:20 +0530

    Change CORS origin and initialize database connection
    Updated CORS origin to allow requests from the production URL and ensured the database connection is properly initialized.

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(
     cors({
         origin: [
-            "http://localhost:5500",
-            "http://127.0.0.1:5500",
-            "http://localhost:5501",
-            "http://127.0.0.1:5501"
+            "http://sk-health-care.netlify.app"
         ]
     })
 );
